5abc525c3b611a087750aa7e9e1b0e1c4be91248
chandrawiguna99 <98540766+chandrawiguna99@users.noreply.github.com>
2022-05-30 16:40:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -42,12 +42,12 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'Chandrapedia'
-global.author = `Punya`
-global.wm = 'Chandra'
+global.packname = 'Punya'
+global.author = `Chandra`
+global.wm = 'ChandraPedia'
 global.watermark = wm
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.instagram = 'https://instagram.com/hyzer-stfu'
+global.instagram = 'https://instagram.com/chndrwgnn_'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
